8255b200d2cca39979e1e517ffd45f8800796103
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-19 16:42:18 -0600

    firebase environmet update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import {useState, useEffect} from 'react';
+import './styles/main.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 function App() {
   return (
---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Axios from 'axios';
+import {useState, useEffect} from 'react';
+import '../styles/main.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+function Login(props) {
+    return(
+        <div className="login">
+            <div className="login-container"></div>
+        </div>
+    )
+}
