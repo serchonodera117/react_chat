568342b209104d9f11c2409e8b7e8a20052ada11
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-19 15:52:28 -0600

    setting environment update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>deploy test</p>
+        <p>deploy test xd</p>
       </header>
     </div>
   );
