8120298b07449cedb5b835845159d49d8e4d8e7c
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-20 19:24:59 -0600

    created login to add users

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import 'firebase/auth';
 
 const firebaseConfig = {
   //your project credentials
+
 };
 
 // Initialize Firebase
@@ -21,17 +22,46 @@ const app = initializeApp(firebaseConfig);
 const database =  getDatabase(app);
 
 function App() {
+  const [isToast, setTsToast] = useState()
   const [db] = useState(database);
   const [isLogged, setisLogged] = useState(false)
+  const [dateToast, setDateToast] = useState("")
+  const [messageToast, setMessageToast] = useState("")
 
   useEffect(()=>{
     let session = localStorage.getItem('isLogged');
     setisLogged(session != null? session : false);
+    setTsToast(false)
   },[])
+
+  function deployToast(title, message){
+    let date = new Date()
+    let formatDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`; 
+    
+    setMessageToast(message)
+    setDateToast(formatDate)
+    setTsToast(true)
+  }
+  function closeToast(){setTsToast(false);}
 
   return (
     <div className="App">
-      {(isLogged)? <Chat db={db}></Chat>: <Login db={db}></Login>}
+      {(isLogged)? <Chat db={db} ></Chat>: <Login db={db} onToast={deployToast}></Login>}
+
+
+      {(isToast)?
+       <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+       <div className="toast-header">
+           <img src={logo} className="rounded me-2 img-toast" alt="..."></img>
+           <strong className="me-auto">Message: </strong>
+           <small>{dateToast}</small>
+           <button type="button" className="btn-close" onClick={closeToast}></button>
+       </div>
+       <div className="toast-body">
+         {messageToast}
+       </div>
+       </div>
+      : <div></div>}
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Axios from 'axios';
 import {useState, useEffect} from 'react';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+
+import defaultUserImage from '../images/default_userimg.png'
 import '../styles/main.css';
 import '../styles/login.css';
 
@@ -9,10 +13,50 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 
-function Login({db}) {
+function Login({db, onToast}) {
+    const [addUserData, setUserData] = useState({username:'', password:'', image:''})
+    const [photoName, setPhotoName] = useState("")
     const dataBase = db;
 
+    useEffect(() => {
+        setUserData({username:'', password:'', image:defaultUserImage})
+        setPhotoName("Profile_Pic(.png/.jpg/.gif/.webp)")
+    },[])
 
+    function activateToast(message){onToast(message);}
+    function photoSelected(e){
+        let reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () =>{
+            let resultImage = reader.result;
+            let photoName = e.target.files[0].name;
+            setUserData(obj=>({...obj, image: resultImage}))
+            setPhotoName(photoName)
+        }
+    }
+    function writeUsername(e){
+        let theUsername = e.target.value;
+        setUserData(item=>({...item, username: theUsername}))
+    }
+    function writePassword(e){
+        let thePassword = e.target.value;
+        setUserData(item=>({...item, password: thePassword}))
+    }
+
+    async function registerUser(e){
+        let db = getFirestore()
+        e.preventDefault();
+        try {
+            const docRef =  await addDoc(collection(db,"users"), addUserData);
+            let checkbox = document.getElementById("input-checkbox-changelogin")
+            checkbox.checked = false;
+            activateToast(`${addUserData.username} se ha registrado con éxito`)
+            setUserData({username:'', password:'', image:defaultUserImage})
+            
+        }catch(error){
+            console.error("Error al insertar usuario : ", error)
+        }
+    }
     return(
         <div className="login">
             <label htmlFor="input-checkbox-changelogin" className="floating-btn">
@@ -20,41 +64,65 @@ function Login({db}) {
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
             </svg>
             </label>
-            <input id="input-checkbox-changelogin" className='hiddenCheckbox' type="checkbox"></input>
+            <input id="input-checkbox-changelogin" className='hiddenItem' type="checkbox"></input>
             
-            <div className="login-container">
-                <h1 className="title-login">LOGIN</h1>
-                <form className="login-form">
-                    <div className="form-row">
-                        <label htmlFor="input-username">Username</label><br></br>
-                        <input className='text-box-login' id="input-username" type="text"></input>
-                    </div>
+            <div className="cards-container">
+                <div className="login-container">
+                    <h1 className="title-login">LOGIN</h1>
+                    <form className="login-form">
+                        <div className="form-row">
+                            <label htmlFor="input-username">Username</label><br></br>
+                            <input className='text-box-login' id="input-username" type="text" ></input>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className="form-row">
+                            <label htmlFor="input-password">Password</label><br></br>
+                            <input className='text-box-login' id="input-password" type="passwprd" ></input>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <button type="submit" className="login-btn">GO</button>
+                    </form>
+                </div>
+
+
+                <div className="register-container">
+                    <h1 className="title-login">Add User</h1>
                     <br></br>
-                    <div className="form-row">
-                        <label htmlFor="input-password">Password</label><br></br>
-                        <input className='text-box-login' id="input-password" type="passwprd"></input>
-                    </div>
-                    <br></br>
-                    <button type="submit" className="login-btn">GO</button>
-                </form>
+                    <form className="login-form" onSubmit={registerUser}>
+                        <div className="form-row">
+                            <img src={addUserData.image} className="img-profile-pic"></img>
+                            <label htmlFor="input-img-user" className='btn-chose-img'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera2" viewBox="0 0 16 16">
+                                    <path d="M5 8c0-1.657 2.343-3 4-3V4a4 4 0 0 0-4 4z"/>
+                                    <path d="M12.318 3h2.015C15.253 3 16 3.746 16 4.667v6.666c0 .92-.746 1.667-1.667 1.667h-2.015A5.97 5.97 0 0 1 9 14a5.972 5.972 0 0 1-3.318-1H1.667C.747 13 0 12.254 0 11.333V4.667C0 3.747.746 3 1.667 3H2a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1h.682A5.97 5.97 0 0 1 9 2c1.227 0 2.367.368 3.318 1zM2 4.5a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0zM14 8A5 5 0 1 0 4 8a5 5 0 0 0 10 0z"/>
+                                </svg>
+                            </label>
+                            <br></br>
+                            <input type="file"  className="hiddenItem" accept='.jpg, .png, .gif, .webp' id='input-img-user'  onChange={photoSelected}></input>
+                            <br></br>
+                            <label>{photoName}</label>
+                        </div>
+                        <br></br>
+                        <div className="form-row">
+                            <label htmlFor="input-add-username">Username</label><br></br>
+                            <input className='text-box-login' id="input-add-username" type="text" onChange={writeUsername} value={addUserData.username}></input>
+                        </div>
+                        <br></br>
+                        <div className="form-row">
+                            <label htmlFor="input-add-password">Password</label><br></br>
+                            <input className='text-box-login' id="input-add-password" onChange={writePassword} value={addUserData.password} type="passwprd"></input>
+                        </div>
+                        <br></br>
+                        <button type="submit" className="login-btn">sign in</button>
+                    </form>
+                </div>
             </div>
             
-            <div className="register-container">
-                <h1 className="title-login">Add User</h1>
-                <form className="login-form">
-                    <div className="form-row">
-                        <label htmlFor="input-username">Username</label><br></br>
-                        <input className='text-box-login' id="input-username" type="text"></input>
-                    </div>
-                    <br></br>
-                    <div className="form-row">
-                        <label htmlFor="input-password">Password</label><br></br>
-                        <input className='text-box-login' id="input-password" type="passwprd"></input>
-                    </div>
-                    <br></br>
-                    <button type="submit" className="login-btn">GO</button>
-                </form>
-            </div>
+
         </div>
     )
 }
